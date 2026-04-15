6ed200f0a3ed13921efcc68149adfbac0be38c11
app <app@app.com>
2026-04-15 21:35:54 +0530

    Removed "DATABASE_URL" default value in settings.py

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 }
 
 CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
-DATABASE_URL = config("DATABASE_URL", cast=str, default="dummy")
+DATABASE_URL = config("DATABASE_URL", cast=str)
 
 if DATABASE_URL is not None:
     tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
