35c333ac272de20f9112ef842c751719f25232f8
app <app@app.com>
2026-04-19 18:11:04 +0530

    Started registration flow

--- src/saas/urls.py
+++ src/saas/urls.py
@@ -22,6 +22,7 @@ from .views import home_view
 urlpatterns = [
     path('', home_view),
     path('login/', auth_views.login_view),
+    path('register/', auth_views.register_view),
     path('home/', home_view),
     path('admin/', admin.site.urls),
 ]
